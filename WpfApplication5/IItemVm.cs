bb9c0706a15e0076bc40df5bb4521f4985c11008
Julien Rosen <jlrosen416@gmail.com>
2016-03-12 08:00:58 -0500

    dd crash

--- WpfApplication5/IItemVm.cs
+++ WpfApplication5/IItemVm.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApplication5
 {
-    public interface IItemVm
+    public interface IItemVm : INotifyPropertyChanged
     {
         string Header { get; set; }
         bool Favorite { get; set; }
--- WpfApplication5/MainWindow.xaml.cs
+++ WpfApplication5/MainWindow.xaml.cs
@@ -1,34 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using DynamicData;
 using DynamicData.Binding;
 using DynamicData.Controllers;
 
 namespace WpfApplication5
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public IObservableCollection<IItemVm> AllItems { get; set; }
-        public IObservableCollection<IItemVm> FavoriteItems { get; set; }
-
         public FilterController<IItemVm> AllFilterController;
-        public FilterController<IItemVm> FavoritesFilterController;
-
+        public SortController<IItemVm> SortController;
+        public MySorter MySorter;
         public SourceCache<IItemVm, int> ItemCache { get; }
 
         public MainWindow()
@@ -38,35 +22,66 @@ namespace WpfApplication5
             DataContext = this;
 
             AllItems = new ObservableCollectionExtended<IItemVm>();
-            FavoriteItems = new ObservableCollectionExtended<IItemVm>();
 
             ItemCache = new SourceCache<IItemVm, int>(itemVm => (int)itemVm.Id);
-            ItemCache.AddOrUpdate(new ItemVm(1));
-            ItemCache.AddOrUpdate(new ItemVm(2));
-            ItemCache.AddOrUpdate(new ItemVm(3));
-            ItemCache.AddOrUpdate(new ItemVm(4));
-            ItemCache.AddOrUpdate(new ItemVm(5));
 
-            FavoritesFilterController = new FilterController<IItemVm>(x => x.Favorite);
+            for (var i = 0; i < 345; i++)
+            {
+                ItemCache.AddOrUpdate(new ItemVm(i));
+            }
+
+            MySorter = new MySorter();
             AllFilterController = new FilterController<IItemVm>(x => !string.IsNullOrEmpty(x.Header));
+            SortController = new SortController<IItemVm>(MySorter);
 
-            var allFilterCache = ItemCache.Connect()
-                .Filter(AllFilterController)
-                .AsObservableCache();
-
-            allFilterCache
-                        .Connect()
-                        .Bind(AllItems)
-                        .Subscribe();
-
-            var favoritesFilterCache = ItemCache.Connect()
-                .Filter(FavoritesFilterController)
-                .AsObservableCache();
-
-            favoritesFilterCache
+            ItemCache
                 .Connect()
-                .Bind(FavoriteItems)
+                .Filter(AllFilterController)
+                .Sort(SortController)
+                .Top(2000)
+                .Bind(AllItems)
                 .Subscribe();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MySorter.SortOption = "Header";
+            MySorter.SortDirection = MySorter.SortDirection == SortDirection.Ascending
+                ? SortDirection.Descending
+                : SortDirection.Ascending;
+            SortController.Resort();
+        }
+
+        private void IdClicked(object sender, RoutedEventArgs e)
+        {
+            MySorter.SortOption = "Id";
+            MySorter.SortDirection = MySorter.SortDirection == SortDirection.Ascending
+                ? SortDirection.Descending
+                : SortDirection.Ascending;
+            SortController.Resort();
+        }
+    }
+
+    public class MySorter : IComparer<IItemVm>
+    {
+        public string SortOption;
+        public SortDirection SortDirection;
+
+        public int Compare(IItemVm x, IItemVm y)
+        {
+            if (SortOption == "Id")
+            {
+                if (SortDirection == SortDirection.Ascending)
+                {
+                    return x.Id.CompareTo(y.Id);
+                }
+                return x.Id.CompareTo(y.Id) * -1;
+            }
+            if (SortDirection == SortDirection.Ascending)
+            {
+                return string.Compare(x.Header, y.Header, StringComparison.Ordinal);
+            }
+            return string.Compare(x.Header, y.Header, StringComparison.Ordinal) * -1;
         }
     }
 }
